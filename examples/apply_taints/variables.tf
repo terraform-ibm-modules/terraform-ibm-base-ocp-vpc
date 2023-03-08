2de5c04137e63acb996818ceb7259c7396e678c2
imprateeksh <sharma_prateek@outlook.com>
2023-03-08 18:58:10 +0530

    feat: worker pool taints are now optional and have no value by default (#42)

--- examples/apply_taints/variables.tf
+++ examples/apply_taints/variables.tf
@@ -4,20 +4,14 @@
 
 variable "ibmcloud_api_key" {
   type        = string
-  description = "IBM Cloud API key associated with the account."
+  description = "The IBM Cloud api token"
   sensitive   = true
-}
-
-variable "resource_group" {
-  type        = string
-  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
-  default     = null
 }
 
 variable "prefix" {
   type        = string
   description = "Prefix for name of all resource created by this example"
-  default     = "base-ocp-sz"
+  default     = "base-ocp-std"
   validation {
     error_message = "Prefix must begin and end with a letter and contain only letters, numbers, and - characters."
     condition     = can(regex("^([A-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix))
@@ -26,13 +20,13 @@ variable "prefix" {
 
 variable "region" {
   type        = string
-  description = "Region where resources are created."
-  default     = "us-east"
+  description = "Region where resources are created"
+  default     = "eu-gb"
 }
 
-variable "ocp_version" {
+variable "resource_group" {
   type        = string
-  description = "Version of the OCP cluster to provision."
+  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
   default     = null
 }
 
@@ -40,6 +34,12 @@ variable "resource_tags" {
   type        = list(string)
   description = "Optional list of tags to be added to created resources"
   default     = []
+}
+
+variable "ocp_version" {
+  type        = string
+  description = "Version of the OCP cluster to provision"
+  default     = null
 }
 
 ##############################################################################
@@ -125,6 +125,58 @@ variable "subnets" {
         cidr     = "10.30.10.0/24"
       }
     ]
+  }
+}
+
+variable "worker_pools" {
+  type = list(object({
+    subnet_prefix     = string
+    pool_name         = string
+    machine_type      = string
+    workers_per_zone  = number
+    resource_group_id = optional(string)
+    labels            = optional(map(string))
+  }))
+  description = "List of worker pools."
+  default = [
+    {
+      subnet_prefix    = "zone-1"
+      pool_name        = "default" # ibm_container_vpc_cluster automatically names standard pool "standard" (See https://github.com/IBM-Cloud/terraform-provider-ibm/issues/2849)
+      machine_type     = "bx2.4x16"
+      workers_per_zone = 2
+    },
+    {
+      subnet_prefix    = "zone-2"
+      pool_name        = "zone-2"
+      machine_type     = "bx2.4x16"
+      workers_per_zone = 2
+    },
+    {
+      subnet_prefix    = "zone-3"
+      pool_name        = "zone-3"
+      machine_type     = "bx2.4x16"
+      workers_per_zone = 2
+    }
+  ]
+}
+
+variable "worker_pools_taints" {
+  type        = map(list(object({ key = string, value = string, effect = string })))
+  description = "Map of lists containing node taints by node-pool name"
+
+  default = {
+    all     = []
+    default = []
+    zone-2 = [{
+      key    = "dedicated"
+      value  = "zone-2"
+      effect = "NoExecute"
+    }]
+    zone-3 = [{
+      key    = "dedicated"
+      value  = "zone-3"
+      effect = "NoExecute"
+    }]
   }
 }
 
