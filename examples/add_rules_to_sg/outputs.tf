0aa95ffa5b1ab9b28a38525ffcb8e6f303a8cd28
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2023-03-14 10:31:08 +0530

    feat: add SG rule example

---
+++ examples/add_rules_to_sg/outputs.tf
@@ -0,0 +1,29 @@
+##############################################################################
+# Outputs
+##############################################################################
+
+output "cluster_name" {
+  value       = module.ocp_base.cluster_name
+  description = "The name of the provisioned cluster."
+}
+
+output "vpc_security_group" {
+  value = data.ibm_security_group.get_vpc_sg
+  description = "Get VPC SG rules"
+}
+
+output "vpc_cluster_security_group" {
+  value = data.ibm_security_group.get_vpc_cluster_sg
+  description = "Get VPC Cluster SG rules"
+}
+
+output "allow_ssh_rule" {
+  value = data.ibm_security_group.allow_ssh
+  description = "Allow SSH rule"
+}
+
+# output "vpc_cluster_security_group" {
+#   value = data.ibm_security_group.get_vpc_cluster_sg
+# }
+
+##############################################################################
