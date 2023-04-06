f8bcb1e945ed335f4da91e7e7cf3a293a554a9b1
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-04-06 15:29:44 -0400

    refactor: read access tags from common-dev permanent yaml

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,16 +2,25 @@
 package test
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"gopkg.in/yaml.v3"
 )
 
 const resourceGroup = "geretain-test-base-ocp-vpc"
 const standardExampleTerraformDir = "examples/standard"
+
+// Define a struct with fields that match the structure of the YAML data
+const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
+
+type Config struct {
+	ExistingAccessTags []string `yaml:"accessTags"`
+}
 
 // Ensure there is one test per supported OCP version
 const ocpVersion1 = "4.12"
@@ -19,15 +28,29 @@ const ocpVersion2 = "4.11"
 const ocpVersion3 = "4.10"
 const ocpVersion4 = "4.9"
 
-var accessTags = []string{"geretain-dev:permanent-test-tag-1", "geretain-dev:permanent-test-tag-2"}
-
 var sharedInfoSvc *cloudinfo.CloudInfoService
+var existingAccessTags []string
 
 // TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
 // for multiple tests
 func TestMain(m *testing.M) {
-
 	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	// Read the YAML file contents
+	data, err := os.ReadFile(yamlLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create a struct to hold the YAML data
+	var config Config
+	// Unmarshal the YAML data into the struct
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Parse the existing access tags from data
+	existingAccessTags = config.ExistingAccessTags
+
 	os.Exit(m.Run())
 }
 
@@ -40,7 +63,7 @@ func setupOptions(t *testing.T, prefix string, terraformDir string) *testhelper.
 		CloudInfoService: sharedInfoSvc,
 		TerraformVars: map[string]interface{}{
 			"ocp_version": ocpVersion1,
-			"access_tags": accessTags,
+			"access_tags": existingAccessTags,
 		},
 	})
 
