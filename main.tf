ca4d594e3062a22856de17eff603a3adeec14603
Aashiq-J <aashiq.jacob@ibm.com>
2023-04-19 23:10:54 +0530

    feat: option to pass list of subnets or subnet prefix

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
 
   # default workers are mapped to the subnets that are "private"
   dynamic "zones" {
-    for_each = var.vpc_subnets[local.default_pool.subnet_prefix]
+    for_each = local.default_pool.subnet_prefix != null ? var.vpc_subnets[local.default_pool.subnet_prefix] : local.default_pool.vpc_subnets
     content {
       subnet_id = zones.value.id
       name      = zones.value.zone
@@ -135,7 +135,7 @@ resource "ibm_container_vpc_cluster" "autoscaling_cluster" {
 
   # default workers are mapped to the subnets that are "private"
   dynamic "zones" {
-    for_each = var.vpc_subnets[local.default_pool.subnet_prefix]
+    for_each = local.default_pool.subnet_prefix != null ? var.vpc_subnets[local.default_pool.subnet_prefix] : local.default_pool.vpc_subnets
     content {
       subnet_id = zones.value.id
       name      = zones.value.zone
@@ -223,7 +223,7 @@ resource "ibm_container_vpc_worker_pool" "pool" {
   kms_account_id    = each.value.boot_volume_encryption_kms_config == null ? null : each.value.boot_volume_encryption_kms_config.kms_account_id
 
   dynamic "zones" {
-    for_each = var.vpc_subnets[each.value.subnet_prefix]
+    for_each = each.value.subnet_prefix != null ? var.vpc_subnets[each.value.subnet_prefix] : each.value.vpc_subnets
     content {
       subnet_id = zones.value.id
       name      = zones.value.zone
@@ -268,7 +268,7 @@ resource "ibm_container_vpc_worker_pool" "autoscaling_pool" {
   }
 
   dynamic "zones" {
-    for_each = var.vpc_subnets[each.value.subnet_prefix]
+    for_each = each.value.subnet_prefix != null ? var.vpc_subnets[each.value.subnet_prefix] : each.value.vpc_subnets
     content {
       subnet_id = zones.value.id
       name      = zones.value.zone
