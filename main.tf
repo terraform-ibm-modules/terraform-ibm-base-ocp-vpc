b5487bd8adfada9276ae66b917d7ee536c99900a
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2023-04-20 06:35:08 +0000

    refactor: leveraged cos module to create the cos instance

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ locals {
   cos_name         = var.use_existing_cos == true || (var.use_existing_cos == false && var.cos_name != null) ? var.cos_name : "${var.cluster_name}_cos"
   cos_location     = "global"
   cos_plan         = "standard"
-  cos_instance_crn = var.use_existing_cos != false ? var.existing_cos_id : ibm_resource_instance.cos_instance[0].id
+  cos_instance_crn = var.use_existing_cos != false ? var.existing_cos_id : module.cos_instance[0].id
 
   # Validation approach based on https://stackoverflow.com/a/66682419
   validate_condition = var.use_existing_cos == true && var.existing_cos_id == null
@@ -38,11 +38,10 @@ module "cos_instance" {
   count = var.use_existing_cos ? 0 : 1
 
   source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-cos.git?ref=v6.1.0"
-  name              = local.cos_name
+  cos_instance_name = local.cos_name
   resource_group_id = var.resource_group_id
-  service           = "cloud-object-storage"
-  plan              = local.cos_plan
-  location          = local.cos_location
+  cos_plan          = local.cos_plan
+  cos_location      = local.cos_location
 }
 
 ##############################################################################
