324f1851ae0ba0884caf372a304c60fc2395a508
Akash Kumar <akash.kumar@ibm.com>
2023-12-07 12:52:35 +0530

    added outputs for the cross acc example

--- examples/cross_kms_support/outputs.tf
+++ examples/cross_kms_support/outputs.tf
@@ -6,3 +6,13 @@ output "cluster_name" {
   value       = module.ocp_base.cluster_name
   description = "The name of the provisioned cluster."
 }
+
+output "kms_cross_account_id" {
+  value       = var.kms_cross_account_id
+  description = "Id of the cross account which owns the KMS instance."
+}
+
+output "kms_instance_guid" {
+  value       = var.kms_instance_guid
+  description = "GUID of the KMS instance existing in the cross account."
+}
