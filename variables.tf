6f0f5f56b73735768171afe6ddd3045d5fb33f3c
ocofaigh <ocofaigh@ie.ibm.com>
2024-05-23 21:16:22 +0100

    feat: add wait_for_apply option in kms_config object input variable

--- variables.tf
+++ variables.tf
@@ -224,6 +224,7 @@ variable "kms_config" {
     instance_id      = string
     private_endpoint = optional(bool, true) # defaults to true
     account_id       = optional(string)     # To attach KMS instance from another account
+    wait_for_apply   = optional(bool, true) # defaults to true so terraform will wait until the KMS is applied to the master, ready and deployed
   })
   description = "Use to attach a KMS instance to the cluster. If account_id is not provided, defaults to the account in use."
   default     = null
