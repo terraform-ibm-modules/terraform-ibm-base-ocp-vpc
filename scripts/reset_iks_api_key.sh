e633a4295e3e759463617ce49445a6ae1847e451
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-05-29 09:20:22 +0530

    update endpoint

--- scripts/reset_iks_api_key.sh
+++ scripts/reset_iks_api_key.sh
@@ -24,7 +24,7 @@ fi
 
 # Login to ibmcloud with cli
 attempts=1
-until ibmcloud login -q -r "${REGION}" -g "${RESOURCE_GROUP_ID}" || [ $attempts -ge 3 ]; do
+until ibmcloud login -q -r "${REGION}" -g "${RESOURCE_GROUP_ID}" -a "cloud.ibm.com" || [ $attempts -ge 3 ]; do
     attempts=$((attempts+1))
     echo "Error logging in to IBM Cloud CLI..." >&2
     sleep 5
