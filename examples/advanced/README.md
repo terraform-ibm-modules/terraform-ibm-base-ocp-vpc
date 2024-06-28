12d494bd77ba8be3330736c09071602f004eb13b
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-06-28 10:11:30 +0200

    fix: fixed advanced example to handle ingress check SKIP UPGRADE TEST

--- examples/advanced/README.md
+++ examples/advanced/README.md
@@ -6,7 +6,7 @@ The following resources are provisioned by this example:
 - A new resource group, if an existing one is not passed in.
 - A Key Protect instance with 2 root keys, one for cluster encryption, and one for worker block storage encryption.
 - A VPC with subnets across 3 zones.
-- A public gateway only in zone-1.
+- A public gateway for all the three zones
 - A multi-zone (3 zone) KMS encrypted OCP VPC cluster, with worker pools in each zone.
 - Auto scaling enabled for the default worker pool.
 - Taints against the workers in zone-2 and zone-3.
--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -63,7 +63,8 @@ resource "ibm_is_public_gateway" "gateway" {
 }
 
 ########################################################################################################################
-# Subnets accross 3 zones (pub gw only attached to zone-1)
+# Subnets accross 3 zones
+# Public gateway attached to all the zones
 ########################################################################################################################
 
 resource "ibm_is_subnet" "subnets" {
@@ -73,8 +74,7 @@ resource "ibm_is_subnet" "subnets" {
   resource_group           = module.resource_group.resource_group_id
   zone                     = "${var.region}-${each.key}"
   total_ipv4_address_count = 256
-  # for this example, gateway only goes on zone-1
-  public_gateway = (each.key == "1") ? ibm_is_public_gateway.gateway.id : null
+  public_gateway           = ibm_is_public_gateway.gateway[each.key].id
 }
 
 ########################################################################################################################
