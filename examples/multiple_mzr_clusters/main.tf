4656ab733f7c27a672834e0047637488d4380860
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-28 22:40:04 -0400

    chore(deps): update terraform-module (#463)

--- examples/multiple_mzr_clusters/main.tf
+++ examples/multiple_mzr_clusters/main.tf
@@ -163,7 +163,7 @@ data "ibm_container_cluster_config" "cluster_config_c2" {
 
 module "observability_instances" {
   source  = "terraform-ibm-modules/observability-instances/ibm"
-  version = "2.13.1"
+  version = "2.13.2"
   providers = {
     logdna.at = logdna.at
     logdna.ld = logdna.ld
@@ -185,7 +185,7 @@ module "observability_instances" {
 
 module "observability_agents_1" {
   source  = "terraform-ibm-modules/observability-agents/ibm"
-  version = "1.25.4"
+  version = "1.26.0"
   providers = {
     helm = helm.helm_cluster_1
   }
@@ -199,7 +199,7 @@ module "observability_agents_1" {
 
 module "observability_agents_2" {
   source  = "terraform-ibm-modules/observability-agents/ibm"
-  version = "1.25.4"
+  version = "1.26.0"
   providers = {
     helm = helm.helm_cluster_2
   }
