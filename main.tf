d9d24cda3939a840158f2e16b5ad5ca679fb25a2
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-08-30 17:39:07 +0530

    test

--- main.tf
+++ main.tf
@@ -315,7 +315,6 @@ data "ibm_container_vpc_worker_pool" "all_pools" {
 }
 
 resource "ibm_container_vpc_worker_pool" "default_pool" {
-  depends_on        = [ibm_container_vpc_worker_pool.pool, ibm_container_vpc_worker_pool.autoscaling_pool]
   vpc_id            = var.vpc_id
   resource_group_id = var.resource_group_id
   cluster           = local.cluster_id
@@ -359,6 +358,7 @@ resource "ibm_container_vpc_worker_pool" "default_pool" {
 }
 
 resource "ibm_container_vpc_worker_pool" "pool" {
+  depends_on        = [ibm_container_vpc_worker_pool.default_pool]
   for_each          = { for pool in local.other_pools : pool.pool_name => pool }
   vpc_id            = var.vpc_id
   resource_group_id = var.resource_group_id
@@ -402,6 +402,7 @@ resource "ibm_container_vpc_worker_pool" "pool" {
 
 # copy of the pool resource above which ignores changes to the worker pool for use in autoscaling scenarios
 resource "ibm_container_vpc_worker_pool" "autoscaling_pool" {
+  depends_on        = [ibm_container_vpc_worker_pool.default_pool]
   for_each          = { for pool in local.other_autoscaling_pools : pool.pool_name => pool }
   vpc_id            = var.vpc_id
   resource_group_id = var.resource_group_id
