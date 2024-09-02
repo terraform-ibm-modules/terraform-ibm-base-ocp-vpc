f52ff52145bfb0568676b763a62f6f36ba538dee
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-09-02 21:38:22 +0530

    test

--- main.tf
+++ main.tf
@@ -315,7 +315,7 @@ data "ibm_container_vpc_worker_pool" "all_pools" {
 }
 
 resource "ibm_container_vpc_worker_pool" "default_pool" {
-  count             = local.default_pool.import_on_create ? 1 : 0
+  count             = local.default_pool.import_on_create != null ? local.default_pool.import_on_create ? 1 : 0 : 0
   vpc_id            = var.vpc_id
   resource_group_id = var.resource_group_id
   cluster           = local.cluster_id
