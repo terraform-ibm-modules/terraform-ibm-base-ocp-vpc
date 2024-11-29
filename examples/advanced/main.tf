947bca5a7edc47f4bf0e8c9529cacefafba43fdd
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-11-29 21:06:53 +0000

    fix(deps): update terraform-module

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -22,7 +22,7 @@ locals {
 
 module "kp_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.16.11"
+  version                   = "4.17.0"
   key_protect_instance_name = "${var.prefix}-kp-instance"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
