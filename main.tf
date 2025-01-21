3ac5ced54dd3bd6c62f91bf670139f49b0f51dc1
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-01-21 08:18:53 +0000

    fix(deps): update terraform-module (#585)

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "cos_instance" {
   count = var.enable_registry_storage && !var.use_existing_cos ? 1 : 0
 
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.16.4"
+  version                = "8.16.5"
   cos_instance_name      = local.cos_name
   resource_group_id      = var.resource_group_id
   cos_plan               = local.cos_plan
