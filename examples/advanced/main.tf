2251f333a218abdf4784991c30a8e12816399961
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-02-07 14:07:22 +0530

    fix: typo correction

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -113,7 +113,7 @@ locals {
       pool_name                         = "default" # ibm_container_vpc_cluster automatically names default pool "default" (See https://github.com/IBM-Cloud/terraform-provider-ibm/issues/2849)
       machine_type                      = "mx2.4x32"
       workers_per_zone                  = 1
-      operating_system                  = "REDHAT_8_64"
+      operating_system                  = "RHEL_9_64"
       enableAutoscaling                 = true
       minSize                           = 1
       maxSize                           = 6
@@ -125,7 +125,7 @@ locals {
       machine_type                      = "bx2.4x16"
       workers_per_zone                  = 1
       secondary_storage                 = "300gb.5iops-tier"
-      operating_system                  = "REDHAT_8_64"
+      operating_system                  = "RHEL_9_64"
       boot_volume_encryption_kms_config = local.boot_volume_encryption_kms_config
     },
     {
