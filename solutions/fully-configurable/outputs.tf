9e3029e19deea3bafb83715e829ef62a071879e6
Md Anam Raihan <md.anam.raihan@ibm.com>
2025-03-10 16:16:08 +0530

    update catalog and add output

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -6,3 +6,8 @@ output "cluster_name" {
   value       = module.ocp_base.cluster_name
   description = "The name of the provisioned cluster."
 }
+
+output "cluster_id" {
+  value       = module.ocp_base.cluster_id
+  description = "The id of the provisioned cluster."
+}
