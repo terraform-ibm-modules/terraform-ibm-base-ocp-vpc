6bcd013f0bb00be036fdf33b9db71af41c9cf7e4
Arya Girish K <arya.girish.k@ibm.com>
2025-03-11 20:09:40 +0530

    Added cluster creation

--- solutions/ocp-ai/main.tf
+++ solutions/ocp-ai/main.tf
@@ -1,49 +1,19 @@
-# Retrieve existing OpenShift cluster details
-data "ibm_container_vpc_cluster" "cluster" {
-  name = var.cluster_name
-}
-# # Retrieve existing  OpenShift cluster version details
-data "ibm_container_cluster_versions" "cluster_versions" {
-  resource_group_id = data.ibm_container_vpc_cluster.cluster.resource_group_id
-}
-# Retrieve existing  existing details
-data "ibm_container_addons" "addons" {
-  cluster = var.cluster_name
-}
-########################################################################################################################
-# VALIDATIONS
-########################################################################################################################
-
-locals {
-
-  worker_pool = { for idx, pool in data.ibm_container_vpc_cluster.cluster.worker_pools : idx => pool if pool.name == var.pool_name }
-  ocp_version = data.ibm_container_cluster_versions.cluster_versions.default_openshift_version
-  kubeconfig  = "../../kubeconfig"
-}
-#tflint-ignore: terraform_unused_declarations
-resource "null_resource" "validations" {
-  provisioner "local-exec" {
-    #command = "scripts/validations.sh ${local.ocp_version} ${local.worker_pool[0].worker_count} ${local.worker_pool[0].flavor} '${join(" ", [for addon in data.ibm_container_addons.addons.addons : addon.name])}' ${local.worker_pool[0].operating_system} ${var.pool_name}"
-    command     = "scripts/validations.sh \"${local.ocp_version}\" ${local.worker_pool[0].worker_count} \"${local.worker_pool[0].flavor}\" \"${join(" ", [for addon in data.ibm_container_addons.addons.addons : addon.name])}\" \"${local.worker_pool[0].operating_system}\" \"${var.pool_name}\""
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      KUBECONFIG = local.kubeconfig
-    }
-  }
+ module "ocp_base" {
+  
+  source                                = "terraform-ibm-modules/base-ocp-vpc/ibm"
+  cluster_name                          = var.cluster_name
+  resource_group_id                     = var.resource_group_id
+  region                                = var.region
+  ocp_version                           = var.ocp_version
+  ocp_entitlement                       = var.ocp_entitlement
+  vpc_id                                = var.vpc_id
+  vpc_subnets                           = var.vpc_subnets
+  worker_pools                          = var.worker_pools
+  disable_outbound_traffic_protection   = true 
+  access_tags                           = var.access_tags   
+  addons = {
+     "openshift-ai"       = "416"
+  } 
+  
 }
 
-
-###################################################################################################################
-#Create OpenShift AI addon
-###################################################################################################################
-resource "ibm_container_addons" "addons" {
-  depends_on = [null_resource.validations]
-
-  cluster           = data.ibm_container_vpc_cluster.cluster.name
-  manage_all_addons = false
-
-  addons {
-    name    = "openshift-ai"
-    version = "416"
-  }
-}
--- solutions/ocp-ai/variables.tf
+++ solutions/ocp-ai/variables.tf
@@ -1,18 +1,96 @@
 variable "ibmcloud_api_key" {
   type        = string
-  description = "The IBM Cloud api key"
+  description = "The IBM Cloud api token"
   sensitive   = true
 }
+
+variable "resource_group_id" {
+  type        = string
+  description = "The Id of an existing IBM Cloud resource group where the cluster will be grouped."
+}
+
 variable "region" {
   type        = string
-  description = "Region where resources are created"
-  default     = "us-south"
+  description = "The IBM Cloud region where the cluster will be provisioned."
 }
+
+# Cluster Variables
+variable "tags" {
+  type        = list(string)
+  description = "Metadata labels describing this cluster deployment"
+  default     = []
+}
+
 variable "cluster_name" {
   type        = string
   description = "The name that will be assigned to the provisioned cluster"
 }
-variable "pool_name" {
+
+variable "vpc_subnets" {
+  type = map(list(object({
+    id         = string
+    zone       = string
+    cidr_block = string
+  })))
+  description = "Metadata that describes the VPC's subnets. Obtain this information from the VPC where this cluster will be created"
+}
+
+variable "worker_pools" {
+  type = list(object({
+    subnet_prefix = optional(string)
+    vpc_subnets = optional(list(object({
+      id         = string
+      zone       = string
+      cidr_block = string
+    })))
+    pool_name         = string
+    machine_type      = string
+    workers_per_zone  = number
+    resource_group_id = optional(string)
+    operating_system  = string
+    labels            = optional(map(string))
+    minSize           = optional(number)
+    secondary_storage = optional(string)
+    maxSize           = optional(number)
+    enableAutoscaling = optional(bool)
+    boot_volume_encryption_kms_config = optional(object({
+      crk             = string
+      kms_instance_id = string
+      kms_account_id  = optional(string)
+    }))
+    additional_security_group_ids = optional(list(string))
+  }))
+  description = "List of worker pools"
+}
+
+variable "ocp_entitlement" {
   type        = string
-  description = "The name that will be assigned to the worker pool"
+  description = "Value that is applied to the entitlements for OCP cluster provisioning"
+  default     = null
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC instance where this cluster will be provisioned"
+}
+
+
+variable "addons" {
+  type = object({
+    openshift-ai = optional(string)
+  })
+  description = "Map of OCP cluster add-on versions to install (NOTE: The 'vpc-block-csi-driver' add-on is installed by default for VPC clusters and 'ibm-storage-operator' is installed by default in OCP 4.15 and later, however you can explicitly specify it here if you wish to choose a later version than the default one). For full list of all supported add-ons and versions, see https://cloud.ibm.com/docs/containers?topic=containers-supported-cluster-addon-versions"
+  default     = {}
+}
+
+variable "access_tags" {
+  type        = list(string)
+  description = "A list of access tags to apply to the resources created by the module, see https://cloud.ibm.com/docs/account?topic=account-access-tags-tutorial for more details"
+  default     = []
+}
+
+variable "ocp_version" {
+  type        = string
+  description = "Version of the OCP cluster to provision"
+  default     = null
 }
