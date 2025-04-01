3430d3669abf69474e4c799bbfaec45e693256e8
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-04-02 02:01:10 +0530

    fix: another round of script review correction

--- tests/scripts/post-validation-destroy-cos-instance-and-vpc.sh
+++ tests/scripts/post-validation-destroy-cos-instance-and-vpc.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
-########################################################################################################################
-## This script is used by the catalog pipeline to destroy the COS instance and VPC, which was provisioned as a            ##
-## prerequisite for the fully-configurable ocp vpc cluster that is published to the catalog                                                ##
-########################################################################################################################
+#############################################################################################################
+# This script is used by the catalog pipeline to destroy the COS instance and VPC, which were provisioned  #
+# as prerequisites for the fully configurable OCP VPC cluster that is published to the catalog.            #
+#############################################################################################################
 
 set -e
 
@@ -11,9 +11,9 @@ TERRAFORM_SOURCE_DIR="tests/existing-resources"
 TF_VARS_FILE="terraform.tfvars"
 
 (
-  cd ${TERRAFORM_SOURCE_DIR}
-  echo "Destroying prerequisite COS instance and VPC .."
-  terraform destroy -input=false -auto-approve -var-file=${TF_VARS_FILE} || exit 1
+  cd "${TERRAFORM_SOURCE_DIR}"
+  echo "Destroying pre-requisite COS instance and VPC..."
+  terraform destroy -input=false -auto-approve -var-file="${TF_VARS_FILE}" || exit 1
 
-  echo "Post-validation completed successfully"
+  echo "Post-validation completed successfully."
 )
--- tests/scripts/pre-validation-deploy-cos-instance-and-vpc.sh
+++ tests/scripts/pre-validation-deploy-cos-instance-and-vpc.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 ############################################################################################################
 ## This script is used by the catalog pipeline to deploy the COS instance and VPC
@@ -10,20 +10,16 @@ set -e
 DA_DIR="solutions/fully-configurable"
 TERRAFORM_SOURCE_DIR="tests/existing-resources"
 JSON_FILE="${DA_DIR}/catalogValidationValues.json"
-REGION="us-south"
 TF_VARS_FILE="terraform.tfvars"
 
 (
   cwd=$(pwd)
   cd ${TERRAFORM_SOURCE_DIR}
-  echo "Provisioning pre-requisite COS instance and VPC .."
+  echo "Provisioning pre-requisite VPC instance .."
   terraform init || exit 1
 
-  # Providing the required inputs to be written into terraform.tfvars file
   # $VALIDATION_APIKEY is available in the catalog runtime
   {
-    echo "ibmcloud_api_key=\"${VALIDATION_APIKEY}\""
-    echo "region=\"${REGION}\""
     echo "prefix=\"ocp-$(openssl rand -hex 2)\""
   } >> ${TF_VARS_FILE}
   terraform apply -input=false -auto-approve -var-file=${TF_VARS_FILE} || exit 1
@@ -32,20 +28,15 @@ TF_VARS_FILE="terraform.tfvars"
   existing_resource_group_value=$(terraform output -state=terraform.tfstate -raw resource_group_name)
   existing_vpc_crn_name="existing_vpc_crn"
   existing_vpc_crn_value=$(terraform output -state=terraform.tfstate -raw vpc_crn)
-  existing_cos_instance_crn_name="existing_cos_instance_crn"
-  existing_cos_instance_crn_value=$(terraform output -state=terraform.tfstate -raw cos_crn)
 
-
-  echo "Appending '${existing_resource_group_name}', '${existing_vpc_crn_name}' and '${existing_cos_instance_crn_name}' input variable values to ${JSON_FILE}.."
+  echo "Appending '${existing_resource_group_name}' and '${existing_vpc_crn_name}' input variable values to ${JSON_FILE}.."
 
   cd "${cwd}"
   jq -r --arg existing_resource_group_name "${existing_resource_group_name}" \
         --arg existing_resource_group_value "${existing_resource_group_value}" \
         --arg existing_vpc_crn_name "${existing_vpc_crn_name}" \
         --arg existing_vpc_crn_value "${existing_vpc_crn_value}" \
-        --arg existing_cos_instance_crn_name "${existing_cos_instance_crn_name}" \
-        --arg existing_cos_instance_crn_value "${existing_cos_instance_crn_value}" \
-        '. + {($existing_resource_group_name): $existing_resource_group_value,($existing_cos_instance_crn_name): $existing_cos_instance_crn_value, ($existing_vpc_crn_name): $existing_vpc_crn_value}' "${JSON_FILE}" > tmpfile && mv tmpfile "${JSON_FILE}" || exit 1
+        '. + {($existing_resource_group_name): $existing_resource_group_value,($existing_vpc_crn_name): $existing_vpc_crn_value}' "${JSON_FILE}" > tmpfile && mv tmpfile "${JSON_FILE}" || exit 1
 
   echo "Pre-validation completed successfully."
 )
