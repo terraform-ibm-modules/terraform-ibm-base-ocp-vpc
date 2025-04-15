3969896c170ba18e0aece788ef5cee31a7b50976
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-04-15 14:22:44 +0530

    update

--- modules/kube-audit/variables.tf
+++ modules/kube-audit/variables.tf
@@ -95,7 +95,7 @@ variable "audit_webhook_listener_image_version" {
   type        = string
   description = "The tag or digest for the audit webhook listener image to deploy. If changing the value, ensure it is compatible with `audit_webhook_listener_image`."
   nullable    = false
-  default     = "122e7a6c887ef24fea3095518857a708bd84b1e4@sha256:04c08891cd0201e11ecc6fe4f7b016ed137989010ced987cab57be5c7af3915a" # datasource: icr.io/ibm/ibmcloud-kube-audit-to-ibm-cloud-logs
+  default     = "deaabcb8225e800385413ba420cf3f819d3b0671@sha256:acf123f4dba63534cbc104c6886abedff9d25a22a34ab7b549ede988ed6e7144" # See, https://github.ibm.com/GoldenEye/issues/issues/13371
   validation {
     condition     = can(regex("^[a-f0-9]{40}@sha256:[a-f0-9]{64}$", var.audit_webhook_listener_image_version))
     error_message = "The value of the audit webhook listener image version must match the tag and sha256 image digest format"
