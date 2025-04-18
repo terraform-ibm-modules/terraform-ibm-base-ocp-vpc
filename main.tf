0f7c2f8df3882e447d81c1e3cf4cf5cc804d67d9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-18 20:32:08 +0000

    fix(deps): update terraform-module

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ module "cos_instance" {
   count = var.enable_registry_storage && !var.use_existing_cos ? 1 : 0
 
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.21.8"
+  version                = "8.21.9"
   cos_instance_name      = local.cos_name
   resource_group_id      = var.resource_group_id
   cos_plan               = local.cos_plan
@@ -718,7 +718,7 @@ locals {
 module "cbr_rule" {
   count            = length(var.cbr_rules) > 0 ? length(var.cbr_rules) : 0
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module"
-  version          = "1.29.0"
+  version          = "1.30.0"
   rule_description = var.cbr_rules[count.index].description
   enforcement_mode = var.cbr_rules[count.index].enforcement_mode
   rule_contexts    = var.cbr_rules[count.index].rule_contexts
