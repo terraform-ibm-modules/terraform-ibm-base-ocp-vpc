c956267e81a4e00c07a518bc0cdb24f5ed12a784
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-27 04:00:37 +0000

    fix(deps): update terraform-module

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos_fscloud" {
   source                        = "terraform-ibm-modules/cos/ibm"
-  version                       = "8.21.15"
+  version                       = "8.21.16"
   resource_group_id             = module.resource_group.resource_group_id
   create_cos_bucket             = false
   cos_instance_name             = "${var.prefix}-cos"
@@ -30,7 +30,7 @@ module "cos_fscloud" {
 
 module "flowlogs_bucket" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "8.21.15"
+  version = "8.21.16"
 
   bucket_configs = [
     {
@@ -52,7 +52,7 @@ module "flowlogs_bucket" {
 module "vpc" {
   depends_on        = [module.flowlogs_bucket]
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.23.0"
+  version           = "7.23.2"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
