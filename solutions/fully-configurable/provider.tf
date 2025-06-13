5ff243d88654016bd1cef99df3f366f54204d812
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-06-13 14:58:51 +0530

    addressed review comments

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -3,21 +3,24 @@
 ########################################################################################################################
 
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = local.vpc_region
-  visibility       = var.provider_visibility
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = local.vpc_region
+  visibility            = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && local.vpc_region == "ca-mon") ? "vpe" : null
 }
 
 provider "ibm" {
-  alias            = "kms"
-  ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
-  region           = local.cluster_kms_region
-  visibility       = var.provider_visibility
+  alias                 = "kms"
+  ibmcloud_api_key      = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
+  region                = local.cluster_kms_region
+  visibility            = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && local.vpc_region == "ca-mon") ? "vpe" : null
 }
 
 provider "ibm" {
-  alias            = "secrets_manager"
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.enable_secrets_manager_integration ? module.existing_secrets_manager_instance_parser[0].region : local.vpc_region
-  visibility       = var.provider_visibility
+  alias                 = "secrets_manager"
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = var.enable_secrets_manager_integration ? module.existing_secrets_manager_instance_parser[0].region : local.vpc_region
+  visibility            = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && local.vpc_region == "ca-mon") ? "vpe" : null
 }
