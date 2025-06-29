bbdcc0fcd42078f76c2f173070bee617470507b0
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-06-29 04:09:58 +0000

    fix(deps): update terraform dependencies

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -38,7 +38,7 @@ module "vpc" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.21.25"
+  version                = "9.1.0"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
