ea69dfcde302162fd7407d5226e072355a557ec8
aatreyee257 <mukherjeeaatreyee25@gmail.com>
2025-07-10 14:20:54 +0530

    resolved commnts

--- modules/kube-audit/variables.tf
+++ modules/kube-audit/variables.tf
@@ -97,7 +97,7 @@ variable "audit_webhook_listener_image_version" {
   nullable    = false
   default     = "deaabcb8225e800385413ba420cf3f819d3b0671@sha256:acf123f4dba63534cbc104c6886abedff9d25a22a34ab7b549ede988ed6e7144"
   validation {
-    condition     = var.audit_webhook_listener_image_version == null || can(regex("^[a-f0-9]{40}@sha256:[a-f0-9]{64}$", var.audit_webhook_listener_image_version))
+    condition     = can(regex("^[a-f0-9]{40}@sha256:[a-f0-9]{64}$", var.audit_webhook_listener_image_version))
     error_message = "The value of the audit webhook listener image version must match the tag and sha256 image digest format"
   }
 }
