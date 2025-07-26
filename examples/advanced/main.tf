46c02523e3f82055516eb6ca45e6628d2ebc7303
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-26 08:19:30 +0000

    fix(deps): update terraform dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -211,7 +211,7 @@ locals {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.5.11"
+  version           = "1.6.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   plan              = "standard"
@@ -245,7 +245,7 @@ module "trusted_profile" {
 module "logs_agents" {
   depends_on                    = [module.kube_audit]
   source                        = "terraform-ibm-modules/logs-agent/ibm"
-  version                       = "1.1.10"
+  version                       = "1.2.0"
   cluster_id                    = module.ocp_base.cluster_id
   cluster_resource_group_id     = module.resource_group.resource_group_id
   logs_agent_trusted_profile_id = module.trusted_profile.trusted_profile.id
