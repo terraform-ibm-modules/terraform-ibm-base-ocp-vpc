40064e298453c247e21344e0abdd2bcb4699fa4c
Arya Girish K <arya.girish.k@ibm.com>
2025-07-31 12:42:00 +0530

    Testing

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -14,6 +14,7 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   type        = string
   nullable    = true
+  default     = null
   description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. Example: prod-0405-ocp. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
 }
 
