dc352107e9311532d3771bd3df47ed1de097bb12
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-08-03 20:08:49 +0100

    fix(deps): update terraform dependencies (#757)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -22,7 +22,7 @@ locals {
 
 module "kp_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.1.14"
+  version                   = "5.1.16"
   key_protect_instance_name = "${var.prefix}-kp-instance"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
@@ -211,7 +211,7 @@ locals {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.6.1"
+  version           = "1.6.3"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   plan              = "standard"
