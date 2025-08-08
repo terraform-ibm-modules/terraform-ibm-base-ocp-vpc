c2a20892b5115aa468fa33f46058221a8ba0ed8f
Arya Girish K <arya.girish.k@ibm.com>
2025-08-08 10:39:41 +0530

    Updated catalog validation,resource_group an output.tf

--- solutions/quickstart/outputs.tf
+++ solutions/quickstart/outputs.tf
@@ -17,6 +17,11 @@ output "cluster_crn" {
   value       = module.ocp_base.cluster_crn
 }
 
+output "vpc_name" {
+  description = "The name of the Virtual Private Cloud (VPC) in which the cluster is deployed."
+  value       = module.vpc.vpc_name
+}
+
 output "vpc_id" {
   description = "The ID of the Virtual Private Cloud (VPC) in which the cluster is deployed."
   value       = module.ocp_base.vpc_id
@@ -25,4 +30,29 @@ output "vpc_id" {
 output "region" {
   description = "The IBM Cloud region where the cluster is deployed."
   value       = module.ocp_base.region
+}
+
+output "cos_crn" {
+  description = "The Cloud Resource Name (CRN) of the Object Storage instance associated with the cluster."
+  value       = module.ocp_base.cos_crn
+}
+
+output "resource_group_id" {
+  description = "The ID of the resource group where the cluster is deployed."
+  value       = module.ocp_base.resource_group_id
+}
+
+output "public_service_endpoint_url" {
+  description = "The public service endpoint URL for accessing the cluster over the internet."
+  value       = module.ocp_base.public_service_endpoint_url
+}
+
+output "master_url" {
+  description = "The API endpoint URL for the Kubernetes master node of the cluster."
+  value       = module.ocp_base.master_url
+}
+
+output "master_status" {
+  description = "The current status of the Kubernetes master node in the cluster."
+  value       = module.ocp_base.master_status
 }
