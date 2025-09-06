166be881ba6c22f0855e25d7694b60073e87c572
Vipin Kumar <77929205+Vipin654@users.noreply.github.com>
2025-09-07 01:40:30 +0530

    Update pr_test.go

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -273,7 +273,7 @@ func TestRoksAddonDefaultConfiguration(t *testing.T) {
 		"fully-configurable",
 		map[string]interface{}{
 			"prefix":                       options.Prefix,
-			"region":                       "eu-de",
+			"region":                       "us-south",
 			"secrets_manager_service_plan": "trial",
 		},
 	)
