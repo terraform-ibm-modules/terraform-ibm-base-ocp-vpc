5d36e0ae6859f9ca411737053b3414c70d16fd01
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-20 03:44:03 +0000

    chore(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.6.29

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -211,7 +211,7 @@ locals {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.6.28"
+  version           = "1.6.29"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   plan              = "standard"
