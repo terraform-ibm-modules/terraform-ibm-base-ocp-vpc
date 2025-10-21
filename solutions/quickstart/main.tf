85f56b3b55f6f76e140d9d7b985203ec726a9aee
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-10-21 13:11:58 +0530

    waypoint test

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -131,7 +131,8 @@ locals {
 # OCP VPC cluster (single zone)
 ########################################################################################################################
 module "ocp_base" {
-  source                              = "../.."
+  source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
+  version                             = "3.66.0"
   cluster_name                        = local.cluster_name
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
