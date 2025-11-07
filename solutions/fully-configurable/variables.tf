877caf4143bdbdf778b74bcb7d6153cd474efe47
Harika-Ponna <Harika.Ponna@ibm.com>
2025-11-07 11:30:49 +0530

    fix: add numeric range validation for default_worker_pool_workers_per_zone and changes prefix to openshift across all flavours

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -176,7 +176,13 @@ variable "default_worker_pool_workers_per_zone" {
   type        = number
   description = "Number of worker nodes in each zone of the cluster."
   default     = 1
+
+  validation {
+    condition     = var.default_worker_pool_workers_per_zone > 0
+    error_message = "Worker count per zone must be greater than 0."
+  }
 }
+
 
 variable "default_worker_pool_operating_system" {
   type        = string
