0f0e45017629c93b517143e2ec52ccfd324101fc
Harika-Ponna <Harika.Ponna@ibm.com>
2025-11-07 14:03:33 +0530

    fix: converted to string to test regex

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -173,14 +173,9 @@ variable "default_worker_pool_machine_type" {
 }
 
 variable "default_worker_pool_workers_per_zone" {
-  type        = number
+  type        = string
   description = "Number of worker nodes in each zone of the cluster."
-  default     = 1
-
-  validation {
-    condition     = var.default_worker_pool_workers_per_zone > 0
-    error_message = "Worker count per zone must be greater than 0."
-  }
+  default     = "1"
 }
 
 
