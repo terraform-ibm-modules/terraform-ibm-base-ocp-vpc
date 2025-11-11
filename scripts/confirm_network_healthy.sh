5405ee9d1dee8b21ff2ff9a020068be749c29fcc
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-11-11 19:36:31 +0530

    alias

--- scripts/confirm_network_healthy.sh
+++ scripts/confirm_network_healthy.sh
@@ -5,7 +5,7 @@ set -e
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod 0755 kubectl
 
-export kubectl="${PATH}/kubectl"
+alias kubectl="${PATH}/kubectl"
 
 function run_checks() {
 
