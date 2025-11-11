f07e3bf615c7c3fb2ed7dc36654e054cfe1e6652
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-11-11 19:35:56 +0530

    test5

--- scripts/confirm_network_healthy.sh
+++ scripts/confirm_network_healthy.sh
@@ -5,9 +5,7 @@ set -e
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod 0755 kubectl
 
-printenv
-
-pwd
+export kubectl="${PATH}/kubectl"
 
 function run_checks() {
 
