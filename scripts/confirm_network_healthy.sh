fc69ad7defa2e783e9d99f932939dfae4c1b694b
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-11-13 14:50:07 +0530

    test

--- scripts/confirm_network_healthy.sh
+++ scripts/confirm_network_healthy.sh
@@ -2,68 +2,68 @@
 
 set -e
 
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-# chmod 0755 kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod 0755 kubectl
 
-# export PATH=$PATH:$PWD
+export PATH=$PATH:$PWD
 
 function run_checks() {
 
-  last_attempt=$1
-  namespace=calico-system
+	last_attempt=$1
+	namespace=calico-system
 
-  MAX_ATTEMPTS=10
-  attempt=0
-  PODS=()
-  while [ $attempt -lt $MAX_ATTEMPTS ]; do
-    # Get list of calico-node pods (There will be 1 pod per worker node)
-    if while IFS='' read -r line; do PODS+=("$line"); done < <(kubectl get pods -n "${namespace}" | grep calico-node | cut -f1 -d ' '); then
-      if [ ${#PODS[@]} -eq 0 ]; then
-        echo "No calico-node pods found. Retrying in 10s. (Attempt $((attempt+1)) / $MAX_ATTEMPTS)"
-        sleep 10
-        ((attempt=attempt+1))
-      else
-        # Pods found, break out of loop
-        break
-      fi
-    else
-      echo "Error getting calico-node pods. Retrying in 10s. (Attempt $((attempt+1)) / $MAX_ATTEMPTS)"
-      sleep 10
-      ((attempt=attempt+1))
-    fi
-  done
+	MAX_ATTEMPTS=10
+	attempt=0
+	PODS=()
+	while [ $attempt -lt $MAX_ATTEMPTS ]; do
+		# Get list of calico-node pods (There will be 1 pod per worker node)
+		if while IFS='' read -r line; do PODS+=("$line"); done < <(kubectl get pods -n "${namespace}" | grep calico-node | cut -f1 -d ' '); then
+			if [ ${#PODS[@]} -eq 0 ]; then
+				echo "No calico-node pods found. Retrying in 10s. (Attempt $((attempt + 1)) / $MAX_ATTEMPTS)"
+				sleep 10
+				((attempt = attempt + 1))
+			else
+				# Pods found, break out of loop
+				break
+			fi
+		else
+			echo "Error getting calico-node pods. Retrying in 10s. (Attempt $((attempt + 1)) / $MAX_ATTEMPTS)"
+			sleep 10
+			((attempt = attempt + 1))
+		fi
+	done
 
-  if [ ${#PODS[@]} -eq 0 ]; then
-    echo "No calico-node pods found after $MAX_ATTEMPTS attempts. Exiting."
-    exit 1
-  fi
+	if [ ${#PODS[@]} -eq 0 ]; then
+		echo "No calico-node pods found after $MAX_ATTEMPTS attempts. Exiting."
+		exit 1
+	fi
 
-  # Iterate through pods to check health
-  healthy=true
-  for pod in "${PODS[@]}"; do
-    command="kubectl logs ${pod} -n ${namespace} --tail=0"
-    # If it is the last attempt then print the output
-    if [ "${last_attempt}" == true ]; then
-      node=$(kubectl get pod "$pod" -n "${namespace}" -o=jsonpath='{.spec.nodeName}')
-      echo "Checking node: $node"
-      if ! ${command}; then
-        healthy=false
-      else
-        echo "OK"
-      fi
-    # Otherwise redirect output to /dev/null
-    else
-      if ! ${command} &> /dev/null; then
-        healthy=false
-      fi
-    fi
-  done
+	# Iterate through pods to check health
+	healthy=true
+	for pod in "${PODS[@]}"; do
+		command="kubectl logs ${pod} -n ${namespace} --tail=0"
+		# If it is the last attempt then print the output
+		if [ "${last_attempt}" == true ]; then
+			node=$(kubectl get pod "$pod" -n "${namespace}" -o=jsonpath='{.spec.nodeName}')
+			echo "Checking node: $node"
+			if ! ${command}; then
+				healthy=false
+			else
+				echo "OK"
+			fi
+		# Otherwise redirect output to /dev/null
+		else
+			if ! ${command} &>/dev/null; then
+				healthy=false
+			fi
+		fi
+	done
 
-  if [ "$healthy" == "false" ]; then
-    return 1
-  else
-    return 0
-  fi
+	if [ "$healthy" == "false" ]; then
+		return 1
+	else
+		return 0
+	fi
 
 }
 
@@ -75,27 +75,27 @@ echo "Running script to ensure kube master can communicate with all worker nodes
 
 while [ ${counter} -le ${number_retries} ]; do
 
-  # Determine if it is last attempt
-  last_attempt=false
-  if [ "${counter}" -eq ${number_retries} ]; then
-    last_attempt=true
-  fi
+	# Determine if it is last attempt
+	last_attempt=false
+	if [ "${counter}" -eq ${number_retries} ]; then
+		last_attempt=true
+	fi
 
-  ((counter=counter+1))
-  if ! run_checks ${last_attempt}; then
-    if [ "${counter}" -gt ${number_retries} ]; then
-      echo "Maximum attempts reached, giving up."
-      echo
-      echo "Found kube master is unable to communicate with one or more of its workers."
-      echo "Please create a support issue with IBM Cloud and include the error message."
-      exit 1
-    else
-      echo "Retrying in ${retry_wait_time}s. (Retry attempt ${counter} / ${number_retries})"
-      sleep ${retry_wait_time}
-    fi
-  else
-    break
-  fi
+	((counter = counter + 1))
+	if ! run_checks ${last_attempt}; then
+		if [ "${counter}" -gt ${number_retries} ]; then
+			echo "Maximum attempts reached, giving up."
+			echo
+			echo "Found kube master is unable to communicate with one or more of its workers."
+			echo "Please create a support issue with IBM Cloud and include the error message."
+			exit 1
+		else
+			echo "Retrying in ${retry_wait_time}s. (Retry attempt ${counter} / ${number_retries})"
+			sleep ${retry_wait_time}
+		fi
+	else
+		break
+	fi
 done
 
 echo "Success! Master can communicate with all worker nodes."
