07a9755f0a8278ebd3053d441853ce7d3e9620d6
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-11-18 20:14:16 +0530

    test

--- examples/basic/hello.py
+++ examples/basic/hello.py
@@ -1,2 +1,19 @@
-# hello.py
-print("Hello from Python inside Terraform!")
+import requests
+import json
+
+url = "https://httpbin.org/post"   # Example test endpoint
+payload = {
+    "message": "Hello from Python!"
+}
+
+response = requests.post(url, json=payload)
+
+# Print HTTP status code
+print("Status:", response.status_code)
+
+# Print JSON response (pretty formatted)
+try:
+    data = response.json()
+    print(json.dumps(data, indent=4))
+except ValueError:
+    print("Response was not JSON.")
