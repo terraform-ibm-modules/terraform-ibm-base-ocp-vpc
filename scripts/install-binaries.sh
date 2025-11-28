f7a4f4c8da0a354135fe5ef6e335d95452035bb3
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-11-28 19:07:04 +0530

    test

--- scripts/install-binaries.sh
+++ scripts/install-binaries.sh
@@ -4,10 +4,18 @@ set -o errexit
 set -o pipefail
 
 DIRECTORY="/tmp"
-JQ_DOWNLOAD_URL=${JQ_DOWNLOAD_URL:-""}
-KUBECTL_DOWNLOAD_URL=${KUBECTL_DOWNLOAD_URL:""}
+TAG=v2.0.1-rc
+
+curl -L "https://github.com/terraform-ibm-modules/common-bash-library/archive/refs/tags/$TAG.tar.gz" -o repo.tar.gz
+mkdir -p "common-bash-library"
+tar -xzf repo.tar.gz --strip-components=1 -C "common-bash-library"
+rm repo.tar.gz
+
 source ./common-bash-library/common/common.sh
 
-install_jq "latest" "${DIRECTORY}" "true" "${JQ_DOWNLOAD_URL}" "true"
+echo "JQ"
+install_jq "latest" "${DIRECTORY}" "true"
+echo "KUBECTL"
+install_kubectl "latest" "${DIRECTORY}" "true"
 
-install_kubectl "latest" "${DIRECTORY}" "true" "${KUBECTL_DOWNLOAD_URL}" "true"
+rm -rf common-bash-library
