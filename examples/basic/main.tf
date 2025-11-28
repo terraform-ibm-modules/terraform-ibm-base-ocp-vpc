69705128c6018da6340af3c50a8c3a458036d70b
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-11-28 10:57:07 +0530

    test

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -69,7 +69,8 @@ locals {
 }
 
 module "ocp_base" {
-  source                              = "../.."
+  source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
+  version                             = "v3.73.3"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.resource_tags
