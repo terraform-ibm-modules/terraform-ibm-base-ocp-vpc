68d9001128f086a47889186daf8332f658bab49d
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-12-01 15:25:09 +0530

    update

--- scripts/install-binaries.sh
+++ scripts/install-binaries.sh
@@ -4,18 +4,19 @@ set -o errexit
 set -o pipefail
 
 DIRECTORY="/tmp"
-TAG=v2.0.1-rc
+TAG=v0.2.0
 
-curl -L "https://github.com/terraform-ibm-modules/common-bash-library/archive/refs/tags/$TAG.tar.gz" -o repo.tar.gz
+echo "Downloading common-bash-library version ${TAG}..!!"
+curl -L "https://github.com/terraform-ibm-modules/common-bash-library/archive/refs/tags/$TAG.tar.gz" -o common-bash.tar.gz &>/dev/null
 mkdir -p "common-bash-library"
-tar -xzf repo.tar.gz --strip-components=1 -C "common-bash-library"
-rm repo.tar.gz
+tar -xzf common-bash.tar.gz --strip-components=1 -C "common-bash-library"
+rm common-bash.tar.gz
 
 source ./common-bash-library/common/common.sh
 
-echo "JQ"
+echo "Installing jq..!!"
 install_jq "latest" "${DIRECTORY}" "true"
-echo "KUBECTL"
+echo "Installing kubectl..!!"
 install_kubectl "latest" "${DIRECTORY}" "true"
 
 rm -rf common-bash-library
