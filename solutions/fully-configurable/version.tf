4651a5e5b89e30f43ffd7a6a1c0a3ce237c4a6ef
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-12 20:51:40 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.86.0"
+      version = "1.86.1"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -13,7 +13,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "3.0.0"
+      version = "3.0.1"
     }
   }
 }
