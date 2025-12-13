aa4fc36d7b88dac7742d09c078a14de27f800202
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-13 15:57:32 +0000

    fix(deps): update dependencies (#891)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -3,7 +3,7 @@
 #############################################################################
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.4.3"
+  version                      = "1.4.5"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
