78ec71d40656b30c3588cdc434570db11f60f1f0
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-12-16 09:58:16 +0530

    test

--- modules/kube-audit/scripts/install-binaries.sh
+++ modules/kube-audit/scripts/install-binaries.sh
@@ -11,10 +11,17 @@ DIRECTORY=${1:-"/tmp"}
 # renovate: datasource=github-tags depName=terraform-ibm-modules/common-bash-library
 TAG=v0.2.0
 
+# use sudo if needed
+arg=""
+if ! [ -w "${DIRECTORY}" ]; then
+    echo "No write permission to ${DIRECTORY}. Using sudo..."
+    arg=sudo
+fi
+
 echo "Downloading common-bash-library version ${TAG}."
 
 # download common-bash-library
-curl --silent \
+${arg} curl --silent \
     --connect-timeout 5 \
     --max-time 10 \
     --retry 3 \
@@ -26,19 +33,19 @@ curl --silent \
     --output "${DIRECTORY}/common-bash.tar.gz" \
     "https://github.com/terraform-ibm-modules/common-bash-library/archive/refs/tags/$TAG.tar.gz"
 
-mkdir -p "${DIRECTORY}/common-bash-library"
-tar --dereference -xzf "${DIRECTORY}/common-bash.tar.gz" --strip-components=1 -C "${DIRECTORY}/common-bash-library"
-rm -f "${DIRECTORY}/common-bash.tar.gz"
+${arg} mkdir -p "${DIRECTORY}/common-bash-library"
+${arg} tar -xzf "${DIRECTORY}/common-bash.tar.gz" --strip-components=1 -C "${DIRECTORY}/common-bash-library"
+${arg} rm -f "${DIRECTORY}/common-bash.tar.gz"
 
 # The file doesn’t exist at the time shellcheck runs, so this check is skipped.
 # shellcheck disable=SC1091
-source "${DIRECTORY}/common-bash-library/common/common.sh"
+${arg} source "${DIRECTORY}/common-bash-library/common/common.sh"
 
 echo "Installing jq."
 install_jq "latest" "${DIRECTORY}" "true"
 echo "Installing kubectl."
 install_kubectl "latest" "${DIRECTORY}" "true"
 
-rm -rf "${DIRECTORY}/common-bash-library"
+${arg} rm -rf "${DIRECTORY}/common-bash-library"
 
 echo "Installation complete successfully"
