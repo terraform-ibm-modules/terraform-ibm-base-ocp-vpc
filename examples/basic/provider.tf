80cb3924b42b6edd9269e559d618af98e2357fa2
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2025-12-17 11:22:29 +0000

    tests: force using user token

--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -3,6 +3,6 @@
 ########################################################################################################################
 
 provider "ibm" {
- # ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
+  # ibmcloud_api_key = var.ibmcloud_api_key
+  region = var.region
 }
