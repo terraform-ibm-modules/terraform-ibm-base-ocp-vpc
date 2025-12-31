d1cfb0b78ffd6e70618f1181ec694e155b4c37d3
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-12-31 17:15:49 +0530

    updates

--- modules/kube-audit/scripts/install-binaries.sh
+++ modules/kube-audit/scripts/install-binaries.sh
@@ -10,6 +10,11 @@ set -o pipefail
 DIRECTORY=${1:-"/tmp"}
 # renovate: datasource=github-tags depName=terraform-ibm-modules/common-bash-library
 TAG=v0.2.0
+# Running multiple Terraform executions on the same environment that share a /tmp directory can lead to conflicts during script execution.
+SUFFIX=""
+for _ in {1..4}; do
+    SUFFIX+=${RANDOM:0:1}
+done
 
 echo "Downloading common-bash-library version ${TAG}."
 
@@ -23,40 +28,22 @@ curl --silent \
     --fail \
     --show-error \
     --location \
-    --output "${DIRECTORY}/common-bash.tar.gz" \
+    --output "${DIRECTORY}/common-bash-${SUFFIX}.tar.gz" \
     "https://github.com/terraform-ibm-modules/common-bash-library/archive/refs/tags/$TAG.tar.gz"
 
-ls -larth /tmp
-
-MAX_RETRIES=5
-ATTEMPT=1
-
-while [ "$ATTEMPT" -le "$MAX_RETRIES" ]; do
-  if tar -xzf "${DIRECTORY}/common-bash.tar.gz" -C "${DIRECTORY}" --no-overwrite-dir; then
-    break
-  fi
-
-  echo "tar extraction failed (attempt ${ATTEMPT}/${MAX_RETRIES})"
-  ATTEMPT=$((ATTEMPT + 1))
-  sleep 2
-done
-
-if [ "$ATTEMPT" -gt "$MAX_RETRIES" ]; then
-  echo "tar extraction failed after ${MAX_RETRIES} attempts"
-  exit 1
-fi
-
-rm -f "${DIRECTORY}/common-bash.tar.gz"
+mkdir -p "${DIRECTORY}/common-bash-library-${SUFFIX}"
+tar -xzf "${DIRECTORY}/common-bash-${SUFFIX}.tar.gz" -C "${DIRECTORY}/common-bash-library-${SUFFIX}"
+rm -f "${DIRECTORY}/common-bash-${SUFFIX}.tar.gz"
 
 # The file doesn’t exist at the time shellcheck runs, so this check is skipped.
 # shellcheck disable=SC1091,SC1090
-source "${DIRECTORY}/common-bash-library-${TAG#v}/common/common.sh"
+source "${DIRECTORY}/common-bash-library-${SUFFIX}/common-bash-library-${TAG#v}/common/common.sh"
 
 echo "Installing jq."
 install_jq "latest" "${DIRECTORY}" "true"
 echo "Installing kubectl."
 install_kubectl "latest" "${DIRECTORY}" "true"
 
-rm -rf "${DIRECTORY}/common-bash-library-${TAG#v}"
+rm -rf "${DIRECTORY}/common-bash-library-${SUFFIX}"
 
 echo "Installation complete successfully"
