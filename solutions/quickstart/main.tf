c13a2424ccba79eb59a66c9be25fe2efd8b447cf
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-01-28 18:44:32 +0530

    test

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -131,7 +131,7 @@ locals {
 # OCP VPC cluster (single zone)
 ########################################################################################################################
 module "ocp_base" {
-  source                              = "../.."
+  source                              = "${path.root}/../.."
   cluster_name                        = local.cluster_name
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
