44baca3bca9048e41af15848c82cc4bac5357e66
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-02-09 13:28:10 +0530

    test2

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -5,3 +5,9 @@ echo '{"name":"alice","age":30}' | jq -r '.name'
 EOT
   }
 }
+
+resource "terraform_data" "kubectl_example" {
+  provisioner "local-exec" {
+    command = "kubectl get pods --all-namespaces"
+  }
+}
