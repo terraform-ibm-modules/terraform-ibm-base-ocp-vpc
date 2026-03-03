35baae40beb205d03071cf33b4fd3632f3e21059
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2026-03-03 11:00:35 +0530

    add sm provider

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -17,6 +17,14 @@ provider "ibm" {
   private_endpoint_type = (var.provider_visibility == "private" && local.vpc_region == "ca-mon") ? "vpe" : null
 }
 
+provider "ibm" {
+  alias                 = "secrets_manager"
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = var.enable_secrets_manager_integration ? module.existing_secrets_manager_instance_parser[0].region : local.vpc_region
+  visibility            = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && local.vpc_region == "ca-mon") ? "vpe" : null
+}
+
 provider "helm" {
   kubernetes = {
     host                   = data.ibm_container_cluster_config.cluster_config[0].host
