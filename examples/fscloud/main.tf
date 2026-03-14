718789e2e7b31530e2e82f7ff07789c237b358f5
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-03-14 20:45:29 +0000

    fix(deps): update dependencies

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -52,7 +52,7 @@ module "flowlogs_bucket" {
 module "vpc" {
   depends_on        = [module.flowlogs_bucket]
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.15.10"
+  version           = "8.15.11"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
