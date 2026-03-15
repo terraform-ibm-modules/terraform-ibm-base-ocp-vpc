a5ff6ed8e972f3a74ee4a57350f334eb0d500570
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-03-15 06:55:10 +0530

    fix(deps): update dependencies (#976)

--- examples/containerized_app_landing_zone/main.tf
+++ examples/containerized_app_landing_zone/main.tf
@@ -765,7 +765,7 @@ locals {
 # Create VPC
 module "vpc" {
   source               = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version              = "8.15.10"
+  version              = "8.15.11"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   create_vpc           = true
@@ -956,7 +956,7 @@ module "trusted_profile" {
 
 module "logs_agent" {
   source                        = "terraform-ibm-modules/logs-agent/ibm"
-  version                       = "1.20.1"
+  version                       = "1.20.2"
   cluster_id                    = module.ocp_base.cluster_id
   cluster_resource_group_id     = module.resource_group.resource_group_id
   logs_agent_trusted_profile_id = module.trusted_profile.trusted_profile.id
