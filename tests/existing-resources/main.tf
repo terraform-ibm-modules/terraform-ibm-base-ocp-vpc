f74d841eb4b38354615460e528b33312e99922f4
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-03-31 13:10:49 +0530

    remove parameter

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -60,6 +60,5 @@ module "cos" {
   cos_instance_name      = "${var.prefix}-cos"
   cos_tags               = var.resource_tags
   bucket_name            = "${var.prefix}-bucket"
-  retention_enabled      = false
   kms_encryption_enabled = false
 }
