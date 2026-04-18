b95fb1924cbda32dae32f4dfdf18c8acfcf019d1
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-04-18 04:14:29 +0100

    fix(deps): update dependencies (#989)

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "2.0.0"
+      version = "2.0.2"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -13,7 +13,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "3.0.1"
+      version = "3.1.0"
     }
   }
 }
--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -67,7 +67,7 @@ locals {
 
 module "vpc" {
   source              = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version             = "8.17.0"
+  version             = "8.17.1"
   resource_group_id   = module.resource_group.resource_group_id
   region              = var.region
   name                = "vpc"
