fcdd765c865e4f02ca20771bb6245834f291b187
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-11 00:05:24 +0000

    fix(deps): update dependencies

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -67,7 +67,7 @@ locals {
 
 module "vpc" {
   source              = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version             = "9.1.0"
+  version             = "9.1.1"
   resource_group_id   = module.resource_group.resource_group_id
   region              = var.region
   name                = "vpc"
