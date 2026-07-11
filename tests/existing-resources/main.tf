f713561ac9880fcc8ef0f7d5151e78753ef003c2
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-07-11 12:46:41 +0530

    fix(deps): update dependencies (#1018)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -38,7 +38,7 @@ locals {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "9.1.0"
+  version           = "9.1.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   name              = "vpc"
@@ -79,7 +79,7 @@ module "vpc" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.2"
+  version                = "10.17.3"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
