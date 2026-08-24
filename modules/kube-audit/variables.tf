d754e33a6d186f6408747804122a0b9d136b66e0
root <root@pipel81a90a5f5dc5799a9bf855bd5412dd45e48be28db3e8650a8f2c4b-pod>
2026-08-24 05:42:52 +0000

    fix(deps): update dependencies

--- modules/kube-audit/variables.tf
+++ modules/kube-audit/variables.tf
@@ -101,7 +101,7 @@ variable "audit_webhook_listener_image_tag_digest" {
   type        = string
   description = "The tag or digest for the audit webhook listener image to deploy. If changing the value, ensure it is compatible with `audit_webhook_listener_image`."
   nullable    = false
-  default     = "16feaff7cf8314b5c8bffed7351a4e9e3c0aa7ba@sha256:5b802197bf1e6453e3e2413e2913af5d7242ed5d464f66bbeb2bb191462f8baf"
+  default     = "3a499618ad7a37415538750b7a2eace04ce746a3@sha256:c5e67bb770e24fa0f7c16cc89eb4d1003e991e757b662d223d9e1f1d437b2a13"
 
   validation {
     condition     = can(regex("^[a-f0-9]{40}@sha256:[a-f0-9]{64}$", var.audit_webhook_listener_image_tag_digest))
