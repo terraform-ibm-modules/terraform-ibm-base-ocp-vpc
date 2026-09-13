d7a0b9f9e8dbbd038dfa5afa8ec4df2022426810
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-13 03:47:05 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -240,7 +240,7 @@ locals {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.15.7"
+  version           = "1.15.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   plan              = "standard"
--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -79,7 +79,7 @@ module "vpc" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.13"
+  version                = "10.17.14"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
