2bd8f9bba6959427da4176202a33740e27e485c9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-19 15:38:56 +0000

    fix(deps): update dependencies

--- examples/virtualization/main.tf
+++ examples/virtualization/main.tf
@@ -38,7 +38,7 @@ locals {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "10.0.6"
+  version           = "10.0.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   name              = "vpc"
